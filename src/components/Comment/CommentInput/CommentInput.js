c2d430079763d7ed081a95994097e4d08d94c6aa
MattSzm <mateuszszmal@gmail.com>
2020-08-13 22:16:25 +0200

    input > textarea

--- src/components/Comment/CommentInput/CommentInput.js
+++ src/components/Comment/CommentInput/CommentInput.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 import classes from './CommentInput.module.css';
 
 const CommentInput = (props) =>{
     return (
         <div>
-            <input type="" value={props.value}
+            <textarea value={props.value}
                    onChange={props.onchangefunc}
                    placeholder="Write Comment"
                    className={classes.CommentInput}/>
